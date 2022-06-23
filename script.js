1a7cb4227bb7dcb7aa39c1af7589eec80e668b46
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-06-24 00:19:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+ class Animal {
+        constructor(name, birthType, color){
+            this.name = name
+            this.birthType = birthType
+            this.color = color
+        }
+    }
